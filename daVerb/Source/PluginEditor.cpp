48f982866cba22f321796819f44e09406ba57ca4
deepio <alex.daigle2@gmail.com>
2019-04-25 00:47:34 -0400

    Added: Background to daVerb

--- daVerb/Source/PluginEditor.cpp
+++ daVerb/Source/PluginEditor.cpp
@@ -15,9 +15,7 @@
 DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     : AudioProcessorEditor (&p), processor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (600, 300);
+    setSize(640, 426);
     const int sliderBoxWidth = 50;
     const int sliderBoxHeight= 20;
 
@@ -26,6 +24,7 @@ DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     roomSizeSlider.setTextBoxStyle(Slider::TextBoxBelow, false, sliderBoxWidth, sliderBoxHeight);
     addAndMakeVisible(roomSizeLabel);
     roomSizeLabel.setText(ROOM_SIZE_NAME, dontSendNotification);
+    roomSizeLabel.setColour(Label::textColourId, Colours::black);
     roomSizeLabel.setJustificationType(Justification::centred);
     roomSizeLabel.attachToComponent(&roomSizeSlider, false);
     roomSizeAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.mParameterTree, ROOM_SIZE_ID, roomSizeSlider);
@@ -35,6 +34,7 @@ DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     dampingSlider.setTextBoxStyle(Slider::TextBoxBelow, false, sliderBoxWidth, sliderBoxHeight);
     addAndMakeVisible(dampingLabel);
     dampingLabel.setText(DAMPING_NAME, dontSendNotification);
+    dampingLabel.setColour(Label::textColourId, Colours::black);
     dampingLabel.setJustificationType(Justification::centred);
     dampingLabel.attachToComponent(&dampingSlider, false);
     dampingAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.mParameterTree, DAMPING_ID, dampingSlider);
@@ -44,6 +44,7 @@ DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     widthSlider.setTextBoxStyle(Slider::TextBoxBelow, false, sliderBoxWidth, sliderBoxHeight);
     addAndMakeVisible(widthLabel);
     widthLabel.setText(WIDTH_NAME, dontSendNotification);
+    widthLabel.setColour(Label::textColourId, Colours::black);
     widthLabel.setJustificationType(Justification::centred);
     widthLabel.attachToComponent(&widthSlider, false);
     widthAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.mParameterTree, WIDTH_ID, widthSlider);
@@ -53,6 +54,7 @@ DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     wetLevelSlider.setTextBoxStyle(Slider::TextBoxBelow, false, sliderBoxWidth, sliderBoxHeight);
     addAndMakeVisible(wetLevelLabel);
     wetLevelLabel.setText(WET_LEVEL_NAME, dontSendNotification);
+    wetLevelLabel.setColour(Label::textColourId, Colours::black);
     wetLevelLabel.setJustificationType(Justification::centred);
     wetLevelLabel.attachToComponent(&wetLevelSlider, false);
     wetLevelAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.mParameterTree, WET_LEVEL_ID, wetLevelSlider);
@@ -62,9 +64,16 @@ DaVerbAudioProcessorEditor::DaVerbAudioProcessorEditor (DaVerbAudioProcessor& p)
     dryLevelSlider.setTextBoxStyle(Slider::TextBoxBelow, false, sliderBoxWidth, sliderBoxHeight);
     addAndMakeVisible(dryLevelLabel);
     dryLevelLabel.setText(DRY_LEVEL_NAME, dontSendNotification);
+    dryLevelLabel.setColour(Label::textColourId, Colours::black);
     dryLevelLabel.setJustificationType(Justification::centred);
     dryLevelLabel.attachToComponent(&dryLevelSlider, false);
     dryLevelAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.mParameterTree, DRY_LEVEL_ID, dryLevelSlider);
+
+//    getLookAndFeel().setColour(Slider::backgroundColourId, Colours::black);
+//    getLookAndFeel().setColour(Slider::rotarySliderFillColourId, Colours::white);
+//    getLookAndFeel().setColour(Slider::rotarySliderOutlineColourId, Colours::black);
+    getLookAndFeel().setColour(Slider::thumbColourId, Colours::white);
+//    titleLabel.setColour (Label::textColourId, Colours::lightgreen);
 }
 
 DaVerbAudioProcessorEditor::~DaVerbAudioProcessorEditor()
@@ -74,8 +83,8 @@ DaVerbAudioProcessorEditor::~DaVerbAudioProcessorEditor()
 //==============================================================================
 void DaVerbAudioProcessorEditor::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (Colours::black);
+    Image background = ImageCache::getFromMemory(BinaryData::bg_jpg, BinaryData::bg_jpgSize);
+    g.drawImageAt(background, 0, 0);
 }
 
 void DaVerbAudioProcessorEditor::resized()
@@ -83,7 +92,7 @@ void DaVerbAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
     const int buttonSize = 80;
-    Rectangle<int> area(10, 30, getWidth(), getHeight() - 60);
+    Rectangle<int> area(10, 30, getWidth(), getHeight() - 290);
     roomSizeSlider.setBounds(area.removeFromLeft(buttonSize));
     dampingSlider.setBounds(area.removeFromLeft(buttonSize));
     widthSlider.setBounds(area.removeFromLeft(buttonSize));
